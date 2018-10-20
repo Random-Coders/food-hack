93e571ff005fac5f2cf08a5162dc00dd3e84a184
calee14 <caplee1000@gmail.com>
2018-10-20 13:53:13 -0700

    make https request to api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Flask APP
-
+import requests
 from flask import Flask, render_template, send_from_directory, request
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -9,7 +10,11 @@ def home():
 
 @app.route("/findrecipe/<food>", methods=["GET", "POST"])
 def generate(food):
-	print(food)
+	# print(f'http://www.recipepuppy.com/api/?q={food}')
+	r = requests.get(f'http://www.recipepuppy.com/api/?q={food}', auth=('user', 'pass'))
+	# return r.json()
+	print(r.json())
 	return render_template('index.html')
+
 if __name__ == "__main__":
 	app.run()

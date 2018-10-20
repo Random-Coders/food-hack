3be085cc84465cb6d55dfb80e325f56363fc18b7
calee14 <caplee1000@gmail.com>
2018-10-20 13:44:09 -0700

    getting food from url

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 #Flask APP
+
+from flask import Flask, render_template, send_from_directory, request
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+	return render_template("index.html")
+
+@app.route("/findrecipe/<food>", methods=["GET", "POST"])
+def generate(food):
+	print(food)
+	return render_template('index.html')
+if __name__ == "__main__":
+	app.run()
